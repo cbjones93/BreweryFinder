40eca96855233c5a14c491283e163f844fef980f
Cody_Jones <cbjones93@gmail.com>
2021-04-26 13:54:09 -0500

    css for homepage

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {Link} from 'react-router-dom'
+import './Home.css'
 export const Home = () => {
     useEffect (() =>{
 
@@ -14,6 +15,19 @@ export const Home = () => {
                 <p>
                 To start off, we recommend creating a profile, that way you can save breweries that interest you and make plans to visit them later. You can make a profile <Link className="navbar__link" to="/Register">Here</Link>
                 </p>
+                <p>
+                When you have created your profile, you can start off with our BreweryFinder&trade;. </p>
+                <p>After you select a state and a city, a list of breweries will be listed. You can click on a brewery to find out more information about it, as well as see reviews from other users to see if you would be interested in visiting.</p>
+               <p>
+               You can also follow other users to keep track of their favorite breweries as well as see their reviews from them. To follow a user you can either add them from a brewery's review section, or to search for them by name. To see your following list click Here
+               </p>
+               <p>
+               Here you will be able to view the people you are following, as well
+as see people that follow you, and you will have the option to follow them back. 
+               </p>
+               <p>
+               We hope you can find amazing breweries and have fun! 
+               </p>
                 </div>
         </div>
         </>
