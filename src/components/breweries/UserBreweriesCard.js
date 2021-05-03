d7b3febe93021294b6b803e8a0621830e907e275
Cody_Jones <cbjones93@gmail.com>
2021-05-03 16:56:59 -0500

    need to go into useState array

--- src/components/breweries/UserBreweriesCard.js
+++ src/components/breweries/UserBreweriesCard.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { Link, useHistory } from 'react-router-dom';
 import { ReviewForm } from "../mybreweries/BreweryReviewForm";
-import {updateUserBrewery, getUserBreweryRelationship} from '../modules/UserBreweryManager'
+import { updateUserBrewery, getUserBreweryRelationship } from '../modules/UserBreweryManager'
 
 
-export const UserBreweryCard = ({brewery}) => {
+export const UserBreweryCard = ({ brewery }) => {
     const currentUser = parseInt(sessionStorage.getItem("app_user_id"));
+    const [userToBrewery, setUserToBrewery] = useState([])
+    const [isLoading, setIsLoading] = useState(false);
     const history = useHistory();
     // const toggleVisited = breweryObj =>{
     //     const breweryToEdit = {...breweryObj}
@@ -13,18 +15,39 @@ export const UserBreweryCard = ({brewery}) => {
     //     updateUserBrewery(breweryToEdit)
     //     .then(getUserBreweries)
     // }
-// get brewery user relationship into state
-useEffect(()=>{
-    getUserBreweryRelationship(brewery.id,currentUser).then(data=>{
-        console.log(data)
-    })
-},[brewery])
-    console.log(brewery)
+    // get brewery user relationship into state
+    useEffect(() => {
+        getUserBreweryRelationship(brewery.id, currentUser).then(UsersFromAPI => {
+            setUserToBrewery(UsersFromAPI);
+            setIsLoading(false);
+        })
+    }, [brewery])
+    // console.log(userToBrewery[0].beenToBrewery)
     return (
         <>
-        Text
+        {userToBrewery.beenToBrewery === true ? 
+           
+            <div className="userBreweryCard">
+            <div className="card-content">
+                <h3>Your Review:</h3>
+                <p>{userToBrewery.review}</p>
+            </div>
+        </div>
+        :<div></div>}
         </>
-    )
+    )    // if (userToBrewery.[0].beenToBrewery === true && userToBrewery[0].review.length > 0) {
+    //     return (
+    //         <div className="userBreweryCard">
+    //             <div className="card-content">
+    //                 <h3>Your Review:</h3>
+    //                 <p>{userToBrewery.review}</p>
+    //             </div>
+    //         </div>
+    //     )
+    // }
+    // else {
+    //     <div>test</div>
+    // }
 
     // if (user.beenToBrewery === true && user.review.length > 0 && user.userId === currentUser) {
     //     return (
@@ -49,7 +72,7 @@ useEffect(()=>{
 
     // else{
     //     return (
-            
+
     //         <div>
     //         <button className="addToFavorites" type="button" onClick={() => handleAddToBreweriesIveBeen(user.breweryId)}>I've been here!</button>
     //         <button className="addToFavorites" type="button" onClick={() => handleAddToBreweriesToVisit(user.breweryId)}>I wanna go here!</button>
